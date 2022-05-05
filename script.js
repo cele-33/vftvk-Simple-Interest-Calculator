c1de13e6247a7cf0a5cf206b4352d5350a0420fa
Cecilia Leung <celeung33@gmail.com>
2022-05-05 07:51:43 +0000

    Interest calculator completed

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    //Validate principal entered should be positive number
+    var principal = document.getElementById("principal").value;
+    if (principal <= 0) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var amount = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+
+    document.getElementById("result").innerHTML = 
+        "If you deposit \<mark\>" + principal + 
+        "\<\/mark\>,\<br\> at an interest rate of \<mark\>" + rate + 
+        "%\<\/mark\>,\<br\> you will receive an amount of \<mark\>" + amount + 
+        "\<\/mark\>,\<br\>in the year \<mark\>" + year + "\<\/mark\>\<br\>";
 }
-        
+
+// Update Interest Rate text string
+function updateRate() {
+
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerHTML = rateval;
+
+}
+
